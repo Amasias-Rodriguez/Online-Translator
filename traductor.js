2b5224b797a2dafaa59aec4446ad10d87516af3c
Rod-aDev <100006082+Rod-aDev@users.noreply.github.com>
2022-10-02 23:22:53 -0600

    Update traductor.js

--- traductor.js
+++ traductor.js
@@ -13,6 +13,9 @@ const OPTIONS = {
 	}
 }
 
+let source_language = 'es';
+let target_language = 'af';
+
 fetch(GET_URL, OPTIONS)
     .then(res => res.json())
     .then(objeto => { 
@@ -23,6 +26,14 @@ fetch(GET_URL, OPTIONS)
            translateFrom.innerHTML += `<option value="1">${element.name}</option>`
            translateTo.innerHTML += `<option value="1">${element.name}</option>`
         })
+        translateFrom.addEventListener('click', ()=>{
+            console.log(translateFrom.value);
+            source_language = translateFrom.value;
+        });
+        translateTo.addEventListener('click', ()=>{
+            console.log(translateTo.value);
+            target_language = translateTo.value;
+        });
     })
     .catch(err => console.log(err))
 
@@ -30,7 +41,34 @@ fetch(GET_URL, OPTIONS)
 
     // Recolect input data and send it to server
     let translate = document.querySelector('#translate');
-    let inputTranslate = document.querySelector('#inputTranslate');
-    translate.addEventListener('click', ()=>{
+    let translateto = document.querySelector('#outputTranslate');
 
-    })
+
+    translate.addEventListener('click', ()=>{
+    let inputTranslate = document.querySelector('#inputTranslate');
+    let textToTranslate = inputTranslate.value; 
+
+
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("source_language", source_language);
+    encodedParams.append("target_language", target_language);
+    encodedParams.append("text", textToTranslate);
+
+    const options = {
+	    method: 'POST',
+	    headers: {
+		    'content-type': 'application/x-www-form-urlencoded',
+		    'X-RapidAPI-Key':   'f3cdceacf6msh71c0e4cff5be441p15ed62jsnf06564818a72',
+		    'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+	    },
+	    body: encodedParams
+    };
+
+    fetch('https://text-translator2.p.rapidapi.com/translate', options)
+        .then(response => response.json())
+        .then(response => translateTo.value = response.data.translatedText)
+        .catch(err => console.error(err));
+        });
+
+    
